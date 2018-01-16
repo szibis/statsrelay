981b7d4f43cdb48b1b9520dbb14f8d9d2f8bd348
Slawomir Skowron <szibis@gmail.com>
2018-01-16 21:57:52 +0100

    Testing functions performance and memory allocation improvements

---
+++ benchmark_test.go
@@ -0,0 +1,28 @@
+package main
+
+import "testing"
+
+// genTags()
+func BenchmarkGenTagsWithTag(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		b.SetBytes(2)
+		b.ReportAllocs()
+		genTags("test.metric.gauge:100|g|#test:foo,test2:bar", []string{"env:sandbox", "region:us-east-1", "type:cluster"}, "${new_prefix}.metric.gauge:100|g|#test:foo,test2:bar")
+	}
+}
+func BenchmarkGenTagsWithoutTag(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		b.SetBytes(2)
+		b.ReportAllocs()
+		genTags("test.metric.gauge:100|g", []string{"env:sandbox", "region:us-east-1", "type:cluster"}, "${new_prefix}.metric.gauge:100|g")
+	}
+}
+
+// getMetricName
+func BenchmarkGetMetricName(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		b.SetBytes(2)
+		b.ReportAllocs()
+		getMetricName([]byte("test.metric.gauge:100|g"))
+	}
+}
